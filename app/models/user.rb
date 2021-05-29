d753da126452325e64ef0f6a20516308d26d64f3
Steve Polito <info@stevepolitodesign.com>
2021-05-29 18:15:01 -0400

    Set features on user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  FEATURES = %i[enable_post_meta_description].freeze
+  store :features, accessors: User::FEATURES
 end
