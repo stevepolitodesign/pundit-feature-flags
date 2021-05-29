017d7ee1e1eb117dd3d4ff874b18993411fc51c4
Steve Polito <info@stevepolitodesign.com>
2021-05-29 18:28:16 -0400

    Build policy.

--- app/policies/feature/enable_post_meta_description_policy.rb
+++ app/policies/feature/enable_post_meta_description_policy.rb
@@ -1,4 +1,16 @@
 class Feature::EnablePostMetaDescriptionPolicy < ApplicationPolicy
+  def ceate?
+    user.present? && (user.enable_post_meta_description == true)
+  end
+
+  def permitted_attributes
+    if user.enable_post_meta_description == true
+      [:title, :user_id, :meta_description]
+    else
+      [:title, :user_id]
+    end
+  end
+
   class Scope < Scope
     def resolve
       scope.all
