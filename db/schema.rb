11b410773e5c3f3f34b8800c2f79d8179bd7620d
Steve Polito <info@stevepolitodesign.com>
2021-05-29 18:17:40 -0400

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_215542) do
+ActiveRecord::Schema.define(version: 2021_05_29_221708) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "meta_description"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_05_29_215542) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.json "features", default: {}
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
